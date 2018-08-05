57894f41264c4e8fd0ad129cb4f54eeb9720784e
Daniel Packard <daniel.packard@complexrehabsystems.com>
2018-08-05 12:40:50 -0500

    Fix images in CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,12 @@
 module.exports = {
   plugins: [
     "gatsby-plugin-sass",
-    "gatsby-plugin-netlify-cms",
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
     {
       resolve:"gatsby-plugin-typography",
       options: {
---
+++ src/cms/cms.js
@@ -0,0 +1,2 @@
+import "../styles/cms.scss"
+
