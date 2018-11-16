45aee7a15c2416be763dfa0d03baf8a3a1a821da
Daniel Packard <daniel.packard@complexrehabsystems.com>
2018-11-16 12:19:02 -0600

    minor docs changes

--- src/components/sections.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../styles/header.scss"
-
-export default ({ children }) =>
-    <div>
-
-        {children}
-
-    </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,20 @@
-import React from "react"
-import ReactDOM from 'react-dom'
-import Header from "../components/header"
-import Footer from "../components/footer"
-import Sections from "../components/sections"
-import Markdown from 'markdown-to-jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "../components/header";
+import Footer from "../components/footer";
+import Sections from "../components/sections";
+import Markdown from "markdown-to-jsx";
 
-import UpArrowIcon from "react-icons/lib/fa/arrow-circle-o-up"
+import UpArrowIcon from "react-icons/lib/fa/arrow-circle-o-up";
 
-import "../styles/layout.scss"
-import "../styles/sections.scss"
-import "../styles/table.scss"
+import "../styles/layout.scss";
+import "../styles/sections.scss";
+import "../styles/table.scss";
 
-import logo from "../assets/crs_3d.png"
-//import { watch } from "fs";
+import logo from "../assets/crs_3d.png";
 
-var remark = require('remark'),
-    reactRenderer = require('remark-react');
-
-
+var remark = require("remark"),
+  reactRenderer = require("remark-react");
 
 // UTILITIES
 const debounce = (fn, time) => {
@@ -25,18 +22,24 @@ const debounce = (fn, time) => {
 
   return function() {
     const functionCall = () => fn.apply(this, arguments);
-    
+
     clearTimeout(timeout);
     timeout = setTimeout(functionCall, time);
-  }
-}
+  };
+};
 
-const formatString = (str) => {
-    return str.toLowerCase().split(" ").join('-');
-}
+const formatString = str => {
+  return str
+    .toLowerCase()
+    .split(" ")
+    .join("-");
+};
 
-function hash(s){
-  return s.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);              
+function hash(s) {
+  return s.split("").reduce(function(a, b) {
+    a = (a << 5) - a + b.charCodeAt(0);
+    return a & a;
+  }, 0);
 }
 
 function hideOverlay() {
@@ -45,7 +48,7 @@ function hideOverlay() {
   let layout = document.querySelector(".layout");
   layout.classList.remove("blurred");
   layout.classList.remove("hidden");
-  window.scrollTo(0,0);
+  window.scrollTo(0, 0);
 }
 
 function showOverlay() {
@@ -54,155 +57,173 @@ function showOverlay() {
   let layout = document.querySelector(".layout");
   layout.classList.add("blurred");
   layout.classList.remove("hidden");
-  window.scrollTo(0,0);
+  window.scrollTo(0, 0);
 }
 
 function unlock(e) {
-    e.preventDefault();
-    var password = document.getElementById("password").value;
+  e.preventDefault();
+  var password = document.getElementById("password").value;
 
-    if(hash(password) === 1032675042) {
-        hideOverlay();
-        document.cookie = "authorized=true";
-    }
-    else {
-        let errorMsg = document.getElementById("unlock-failed");
-        errorMsg.classList.remove("hidden");
-        errorMsg.classList.add("shake");
-    }
+  if (hash(password) === 1032675042) {
+    hideOverlay();
+    document.cookie = "authorized=true";
+  } else {
+    let errorMsg = document.getElementById("unlock-failed");
+    errorMsg.classList.remove("hidden");
+    errorMsg.classList.add("shake");
+  }
 
-    return false;
+  return false;
 }
 
-if (typeof window !== 'undefined') {
-  require('smooth-scroll')('a[href*="#"]');
-  window.addEventListener('scroll', debounce(function() {
-    document.getElementById('top-link').style.opacity = (pageYOffset-800);
-    }, 100));
+if (typeof window !== "undefined") {
+  require("smooth-scroll")('a[href*="#"]');
+  window.addEventListener(
+    "scroll",
+    debounce(function() {
+      document.getElementById("top-link").style.opacity = pageYOffset - 800;
+    }, 100)
+  );
 
-    function reRenderTableOfContents(toc) {
-        var tocDiv = document.querySelector(".table-of-contents");
+  function reRenderTableOfContents(toc) {
+    var tocDiv = document.querySelector(".table-of-contents");
 
-        var tocItems = [];
-        const markup = toc.map(item => {
-            const c = "toc-item-" + item.type;
-            const link = formatString(item.value);
-            return `<div class="${c}"><a href="#${link}">${item.value}</a></div>`;
-        }).join("");
+    var tocItems = [];
+    const markup = toc
+      .map(item => {
+        const c = "toc-item-" + item.type;
+        const link = formatString(item.value);
+        return `<div class="${c}"><a href="#${link}">${item.value}</a></div>`;
+      })
+      .join("");
 
-        tocDiv.innerHTML = '<h1 className="table-heading">Table of Contents</h1>' + markup;
-    }
+    tocDiv.innerHTML =
+      '<h1 className="table-heading">Table of Contents</h1>' + markup;
+  }
 
-    var toc = [];
-    function load() {
-        var sectionHeadings = document.querySelectorAll(".section h1, .section h2");
+  var toc = [];
+  function load() {
+    var sectionHeadings = document.querySelectorAll(".section h1, .section h2");
 
-        var lastSection;
-        sectionHeadings.forEach((heading) => {
-            heading.id = formatString(heading.textContent);
-            if (heading.nodeName == "H1") {
-                toc[heading.textContent] = []
-                lastSection = heading.textContent;
-                toc.push({ type: "section", value: heading.textContent });
-            }
+    var lastSection;
+    sectionHeadings.forEach(heading => {
+      heading.id = formatString(heading.textContent);
+      if (heading.nodeName == "H1") {
+        toc[heading.textContent] = [];
+        lastSection = heading.textContent;
+        toc.push({ type: "section", value: heading.textContent });
+      }
 
-            if (heading.nodeName == "H2") {
-                toc[lastSection].push(heading.textContent);
-                toc.push({ type: "sub-section", value: heading.textContent });
-            }
-        });
+      if (heading.nodeName == "H2") {
+        toc[lastSection].push(heading.textContent);
+        toc.push({ type: "sub-section", value: heading.textContent });
+      }
+    });
 
-        let unlockFailed = document.querySelector("#unlock-failed");
-        unlockFailed.addEventListener("animationend", function(e) {
-          this.classList.remove("shake");
-        });
+    let unlockFailed = document.querySelector("#unlock-failed");
+    unlockFailed.addEventListener("animationend", function(e) {
+      this.classList.remove("shake");
+    });
 
-        if(document.cookie)
-          hideOverlay();
-        else 
-          showOverlay();
+    if (document.cookie) hideOverlay();
+    else showOverlay();
 
-        reRenderTableOfContents(toc);
-    }
-    
-    window.onload = load;
+    reRenderTableOfContents(toc);
+  }
+
+  window.onload = load;
 }
 
 // MAIN COMPONENT
-export default ( {data}) => {
+export default ({ data }) => {
   const headerInfo = data.allHeaderYaml.edges[0].node;
   const footerInfo = data.allFooterYaml.edges[0].node;
   const sections = data.allSectionsYaml.edges.map(e => e.node);
 
-    const RenderSection = (sectionInfo, index) => {
-      return <div className="manual-section" id={index}>
-      <h1 className="title">{sectionInfo.title}</h1>
-      <Markdown>{sectionInfo.content}</Markdown>
-    </div>
-    }
-    
-    const RenderTableOfContents = (sectionInfo, index) => {
-        const link = "#" + formatString(sectionInfo.title);
-      return <div className="table-contents">
-          <a href={link}>{sectionInfo.title}</a>
+  const RenderSection = (sectionInfo, index) => {
+    return (
+      <div className="manual-section" id={index}>
+        <h1 className="title">{sectionInfo.title}</h1>
+        <Markdown>{sectionInfo.content}</Markdown>
       </div>
-    }
-    
-    return <div className="container">
-        <div className="layout hidden blurred">
+    );
+  };
+
+  const RenderTableOfContents = (sectionInfo, index) => {
+    const link = "#" + formatString(sectionInfo.title);
+    return (
+      <div className="table-contents">
+        <a href={link}>{sectionInfo.title}</a>
+      </div>
+    );
+  };
+
+  return (
+    <div className="container">
+      <div className="layout hidden blurred">
         <Header className="site-header">
-            <img src={logo} className="logo" />
-            <div className="user-manual-info">
-                <h1>{headerInfo.title}</h1>
-                <h3>{headerInfo.subtitle}</h3>
-                <h3>{headerInfo.publicationDate}</h3>
-            </div>
+          <img src={logo} className="logo" />
+          <div className="user-manual-info">
+            <h1>{headerInfo.title}</h1>
+            <h3>{headerInfo.subtitle}</h3>
+            <p>
+              <b>{headerInfo.publicationDate}</b>
+            </p>
+            <p>
+              <b>{headerInfo.version}</b>
+            </p>
+          </div>
         </Header>
 
         <a id="top-link" href="#">
-          <UpArrowIcon/>
-          <h2>Top</h2>
-          </a>
+          <UpArrowIcon />;<h2>Top</h2>
+        </a>
 
-          {/* Render table of contents */}
-          <div className="table-of-contents">
-            <div className="spinner"></div>
-          </div>
+        {/* Render table of contents */}
+        <div className="table-of-contents">
+          <div className="spinner" />
+        </div>
 
-          {/* Render all sections of the manual from our CMS */}
-          <div className="section">
-            {sections.filter(sectionInfo => sectionInfo.published).sort((sectionInfo1, sectionInfo2) => sectionInfo1.displayOrder > sectionInfo2.displayOrder).map((sectionInfo, index) => RenderSection(sectionInfo, index))}
-          </div>
+        {/* Render all sections of the manual from our CMS */}
+        <div className="section">
+          {sections
+            .filter(sectionInfo => sectionInfo.published)
+            .sort((sectionInfo1, sectionInfo2) => {
+              return sectionInfo1.displayOrder > sectionInfo2.displayOrder;
+            })
+            .map((sectionInfo, index) => RenderSection(sectionInfo, index))}
+        </div>
 
         <Footer className="site-footer">
           <span>&copy; {footerInfo.copyright}</span>
           <span>{footerInfo.address}</span>
         </Footer>
+      </div>
 
-        </div>
-
-        <div className="lockout-overlay hidden">
-          <div className="lockout-bg"></div>
-          <form onSubmit={unlock}>
-            <img src={logo} className="logo" />
-            <input id="password" type="password" placeholder="password"></input>
-            <button type="submit" >Unlock Documentation</button>
-            <p id="unlock-failed" className="hidden">Please try again.</p>
-          </form> 
-        </div>
-
+      <div className="lockout-overlay hidden">
+        <div className="lockout-bg" />
+        <form onSubmit={unlock}>
+          <img src={logo} className="logo" />
+          <input id="password" type="password" placeholder="password" />
+          <button type="submit">Unlock Documentation</button>
+          <p id="unlock-failed" className="hidden">
+            Please try again.
+          </p>
+        </form>
+      </div>
     </div>
-}
+  );
+};
 
 export const query = graphql`
   query AllData {
-  
     allHeaderYaml {
       totalCount
       edges {
         node {
           title
           subtitle
+          version
           publicationDate
         }
       }
@@ -229,6 +250,5 @@ export const query = graphql`
         }
       }
     }
-
   }
-`
+`;
