ec925b337936753f4985b80afcdcd4e754a082ae
Daniel Packard <daniel.packard@complexrehabsystems.com>
2018-11-16 12:21:04 -0600

    minor docs changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "../components/header";
 import Footer from "../components/footer";
-import Sections from "../components/sections";
 import Markdown from "markdown-to-jsx";
 
 import UpArrowIcon from "react-icons/lib/fa/arrow-circle-o-up";
